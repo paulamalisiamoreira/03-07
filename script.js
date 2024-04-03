7153dae8fcbaebab59b8dc546848dbd9c5aa1f7b
paulamalisiamoreira <142233587+paulamalisiamoreira@users.noreply.github.com>
2024-04-03 09:35:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+        function funcaoMudarTamanho() {
+            document.getElementById("test").style.fontSize = "35px";
+
+        }
+
+        function funcaoMudarOBackECorFOnte() {
+            document.getElementById("test").style.color = "red";
+            document.querySelector("body").style.backgroundColor = "pink";
+
+        }
+
+        function conta() {
+            document.getElementById('test').innerHTML = 5 + 8;
+        }
